39463b3b8a5c1c9297d5d3afe667faea3686ec73
diegosrvr <diegoveloso34@hotmail.com>
2017-11-14 15:16:28 -0300

    cambios reunion

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -1,5 +1,4 @@
-<<<<<<< current
-
+﻿
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Main extends CI_Controller {
@@ -69,81 +68,3 @@ function just_a_test($primary_key , $row)
 
 
 }
-
-/* End of file Main.php */
-/* Location: ./application/controllers/Main.php */
-=======
-
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-class Main extends CI_Controller {
-
-function __construct()
-{
-        parent::__construct();
-
-/* Standard Libraries of codeigniter are required */
-$this->load->database();
-$this->load->helper('url');
-/* ------------------ */
-
-$this->load->library('grocery_CRUD');
-
-}
-
-public function index()
-{
-echo "<h1>Welcome to the world of Street Fighter III</h1>";//Just an example to ensure that we get into the function
-die();
-}
-
-public function subidas()
-{
-  $this->load->helper('download');
-
-$this->load->helper('url');
-$crud = new grocery_CRUD();
-$crud->set_theme('datatables');
-$crud->set_language("spanish");
-
-$crud->set_subject('Postulaciones');
-$crud->set_table('postulaciones');
-$crud->columns('practica','nombre','apellidos','rut','region','comuna','direccion','telefono','telefono2','email','universidad','añoegreso','educacion_superior','titulos','cursos','areaesp','areainteres','fecha');
-$crud->add_action('Curriculum', '', '','ui-icon-image',array($this,'just_a_test'));
-$crud->display_as('añoegreso','Año de Egreso');
-$crud->display_as('areaesp','Área de Especialidad');
-$crud->display_as('areainteres','Área de Interés');
-
-$crud->display_as('fecha','Fecha de Postulación');
-
-$crud->unset_read();
-$crud->unset_add();
-$crud->unset_edit();
-$jsfile = 'assets/grocery_crud/js/refresh.js';
-$crud->set_js($jsfile);
-$output = $crud->render();as
-$this->load->helper('file');
-
-$this->_example_output($output);
-}
-
-
-
-function _example_output($output = null)
-{
-$this->load->view('our_template.php',$output);
-}
-
-function just_a_test($primary_key , $row)
-{
-
-  return "../../gitHub/formularioREAL/uploads/".$row->link;
-
-}
-
-
-}
-
-/* End of file Main.php */
-/* Location: ./application/controllers/Main.php */
->>>>>>> before discard
