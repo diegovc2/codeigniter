f0643b7109e3d5b985b5fc40582d2d219176ccd1
diegosrvr <diegoveloso34@hotmail.com>
2017-11-15 15:14:00 -0300

    extraños commit

--- gitHub/formularioREAL/test.php
+++ gitHub/formularioREAL/test.php
@@ -10,14 +10,20 @@ require 'vendor/autoload.php';
 $mail = new PHPMailer(true);                              // Passing `true` enables exceptions
 try {
     //Server settings
+    $mail->SMTPDebug = 4;                                 // Enable verbose debug output
     $mail->isSMTP();                                      // Set mailer to use SMTP
     $mail->Host = '192.168.0.14';  // Specify main and backup SMTP servers
     $mail->SMTPAuth = true;                               // Enable SMTP authentication
     $mail->Username = 'doc@atmsa.cl';                 // SMTP username
     $mail->Password = 'Aiai9884';                           // SMTP password
+<<<<<<< HEAD
     $mail->SMTPSecure = 'false';                            // Enable TLS encryption, `ssl` also accepted
     $mail->Port = 25;                                    // TCP port to connect to
     $mail->CharSet = 'UTF-8';
+=======
+    $mail->SMTPSecure = 'true';                            // Enable TLS encryption, `ssl` also accepted
+    $mail->Port = 587;                                    // TCP port to connect to
+>>>>>>> parent of 7868742... Archivos de Mailer
 
     //Recipients
     $mail->setFrom('doc@atmsa.cl', 'Mailer');
@@ -33,6 +39,7 @@ try {
 
     //Content
     $mail->isHTML(true);                                  // Set email format to HTML
+<<<<<<< HEAD
     $mail->Subject = 'Postulación de '.$name.' '.$fecha;
 <<<<<<< HEAD
     $mail->Body    =
@@ -58,8 +65,15 @@ try {
                     '<p>Especialidades:  '.$areaesp.'<p>'
     ;
 
+=======
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+>>>>>>> parent of 7868742... Archivos de Mailer
 
     $mail->send();
+    echo 'Message has been sent';
 } catch (Exception $e) {
+    echo 'Message could not be sent.';
     echo 'Mailer Error: ' . $mail->ErrorInfo;
 }
